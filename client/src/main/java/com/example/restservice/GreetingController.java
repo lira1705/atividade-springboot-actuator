df93bceee1ee4e22d9530c6770314e053bf51163
José Reginaldo <helres@libro-cpu-003.local>
2020-06-09 23:34:18 -0300

    changes to swagger

--- client/src/main/java/com/example/restservice/GreetingController.java
+++ client/src/main/java/com/example/restservice/GreetingController.java
@@ -11,9 +11,4 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
 }
--- client/src/main/java/com/example/restservice/QuoteController.java
+++ client/src/main/java/com/example/restservice/QuoteController.java
@@ -8,8 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
-import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 @RestController
 public class QuoteController {
 
@@ -18,16 +30,11 @@ public class QuoteController {
 
   RestTemplate restTemplate;
 
-	@GetMapping("/quote")
-	public Quote quote() {
-		return run(restTemplate());
-	}
-
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
-
+	@ApiOperation(value="Retorna uma lista de Estados")
 	@GetMapping("/states")
 	public ArrayList<States> goToStates(){
 		return getAllStates();
@@ -39,6 +46,7 @@ public class QuoteController {
 		return array;
     }
 
+	@ApiOperation(value="Retorna informação de um estado")
 	@GetMapping("/state")
 	public States goToState(@RequestParam(value = "abbreviation", defaultValue = "PE")  String abbreviation){
 		for(States state : getAllStates()) { 
